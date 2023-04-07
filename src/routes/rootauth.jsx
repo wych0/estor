cda1d7f88a2bec16045d7abe379e74c8be321444
wych0 <wycho01@wp.pl>
2023-04-07 15:59:33 +0200

    Register form, userContext

---
+++ src/routes/rootauth.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header'
+import BodyHome from '../components/BodyHome'
+
+
+export default function HomePage() {
+    return (
+      <div className="boxBody">
+        <Header></Header>
+        <BodyHome></BodyHome>
+      </div>
+    );
+  }
