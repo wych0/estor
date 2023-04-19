517e9a0ffecee7090e2e9995a87c32844c31a484
Albert Wychowaniec <wycho01@wp.pl>
2023-04-19 12:09:48 +0200

    Order Details

--- src/components/ItemOrderDetails.jsx
+++ src/components/ItemOrderDetails.jsx
@@ -1,10 +1,10 @@
 function ItemOrderDetails(props) {
     return (
       <div className="boxItemOrderDetails flex wrap">
-        <div className="boxImgCart shadow flex centerX centerY">
+        <div className="boxImgOrderDetails shadow flex centerX centerY">
             <img className="imgItem" src="/images/products/product1.png" alt="produkt"/>
         </div>
-        <div className="boxBrandNameBtnCart flex wrap">
+        <div className="boxItemOrderDetailsProps flex wrap">
           <p className="text itemOrderDetails brand">{props.brand}</p>
           <p className="text itemOrderDetails name">{props.name}</p>
           <p className="text itemOrderDetails price">{props.price} zł</p>
--- src/components/OrderDetails.jsx
+++ src/components/OrderDetails.jsx
@@ -24,30 +24,52 @@ function OrderDetails(props){
     const {user} = useContext(UserContext)
     const orderToDisplay = orders.find(el => el.id===user.displayedOrder);
     const isOrderChosen = props.isOrderChosen==='false' ? false : true
-    const filterData = isOrderChosen ? filterProductsByOrderItems(products, orderToDisplay.products) : ' '
+    if(!isOrderChosen) {
+        return (
+            <div className="box orderDetailsContent flex wrap centerX">
+                <div className="box orderDetailsHeader header flex centerY">
+                    <p className="text orderDetails header">Nie wybrano zamówienia</p>
+                </div>
+                <div className="box displayAlertOrderDetails flex wrap centerX">
+                    <i className="bi bi-info-circle-fill icon detailsBtn info"></i>
+                    <p className="text detailsBtn info">Aby wyświetlić szczegóły danego zamówienia, skorzystaj z przycisku "Szczegóły".</p>
+                </div>
+            </div>
+        )
+    }
+    const filterData = filterProductsByOrderItems(products, orderToDisplay.products)
+    const isBtnDisabled = orderToDisplay.status==="W realizacji" ? false : true
     return(
         <div className="box orderDetailsContent flex wrap centerX">
             <div className="box orderDetailsHeader header flex centerY">
-                <p className="text orderDetails header">{ isOrderChosen ? 'Szczegóły zamówienia' : 'Nie wybrano zamówienia'}</p>
-                {isOrderChosen ? <Button variant="contained" sx={btnStyle}>Anuluj</Button> : ' '}
+                <p className="text orderDetails header">Szczegóły zamówienia</p>
+                <Button variant="contained" sx={btnStyle} disabled={isBtnDisabled}>Anuluj</Button>
             </div>
-            <div className="box orderDetailsInfo flex wrap">
-                <p className="text orderDetails info">Numer zamówienia: <span className="text orderDetails info data">{ isOrderChosen ? orderToDisplay.id : ' '}</span></p>
-                <p className="text orderDetails info">Data zamówienia: <span className="text orderDetails info data">{ isOrderChosen ? orderToDisplay.date : ' '}</span></p>
-                <p className="text orderDetails info">Koszt zamówienia: <span className="text orderDetails info data">{ isOrderChosen ? orderToDisplay.cost : ' '}</span></p>
-                <p className="text orderDetails info">Status zamówienia: <span className="text orderDetails info data">{ isOrderChosen ? orderToDisplay.status : ' '}</span></p>
-            </div>
-            <div className ="orderDetailsItems flex wrap">
-                {filterData===' '
-                ? <p> </p>
-                : filterData.map((item) => (
-                    <div key={item.id} className="listItemCart"><ItemOrderDetails itemId={item.id} brand={item.brand} name={item.name} price={item.price}></ItemOrderDetails></div>
-                    ))
-                } 
-            </div>
-            
+                <div className="box displayOrderDetails flex wrap">
+                    <div className="box orderDetailsInfo flex wrap">
+                        <p className="text orderDetails info">Numer: <span className="text orderDetails info data">{orderToDisplay.id}</span></p>
+                        <p className="text orderDetails info">Data: <span className="text orderDetails info data">{orderToDisplay.date}</span></p>
+                        <p className="text orderDetails info">Koszt: <span className="text orderDetails info data">{orderToDisplay.cost}</span></p>
+                        <p className="text orderDetails info">Status: <span className="text orderDetails info data">{orderToDisplay.status}</span></p>
+                    </div>
+                    <div className="box orderDetailsInfo flex wrap">
+                        <p className="text orderDetails info">Adres dostawy</p>
+                        <p className="text orderDetails info data address">{orderToDisplay.addres.name.concat((' '), orderToDisplay.addres.secName)}</p>
+                        <p className="text orderDetails info data address">{orderToDisplay.addres.street}</p>
+                        <p className="text orderDetails info data address">{orderToDisplay.addres.postalCode.concat((', '), orderToDisplay.addres.city)}</p>
+                        <p className="text orderDetails info data address">{orderToDisplay.addres.country}</p>
+                    </div>
+                    <div className ="orderDetailsItems flex wrap">
+                        <p className="text orderDetails info">Zamówione produkty</p>
+                        {filterData===' '
+                        ? <p> </p>
+                        : filterData.map((item) => (
+                            <div key={item.id} className="listItemOrderDetails"><ItemOrderDetails itemId={item.id} brand={item.brand} name={item.name} price={item.price}></ItemOrderDetails></div>
+                            ))
+                        } 
+                    </div>
+            </div> 
         </div>
-        
     );
 }
 
