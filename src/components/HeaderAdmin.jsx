aab84401286bd8d229bcc2e9318c35b6298d6f95
Albert Wychowaniec <wycho01@wp.pl>
2023-04-20 15:47:53 +0200

    Header Admin

---
+++ src/components/HeaderAdmin.jsx
@@ -0,0 +1,9 @@
+function HeaderAdmin() {
+    return (
+      <div className={"headerContainer flex wrap centerY centerX"}>
+        <img src="images/logoSvg.svg" alt="logoImgHeader" className="logoImgHeader"/>
+      </div>
+    );
+  }
+  
+  export default HeaderAdmin;
