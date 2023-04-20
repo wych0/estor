301b1055e0ee71d8d4e25f8ba9b1482f4c3856a0
Albert Wychowaniec <wycho01@wp.pl>
2023-04-20 15:49:19 +0200

    Admin page file

---
+++ src/routes/admin.jsx
@@ -0,0 +1,11 @@
+import HeaderAdmin from '../components/HeaderAdmin'
+import BodyAdmin from '../components/BodyAdmin';
+
+export default function Admin() {
+    return (
+        <div id="adminPage">
+        <HeaderAdmin></HeaderAdmin>
+        <BodyAdmin></BodyAdmin>
+        </div>
+    );
+}
