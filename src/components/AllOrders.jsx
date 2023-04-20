70aaebc279e3e6e34ab57b214d9ff04a62e2c37c
Albert Wychowaniec <wycho01@wp.pl>
2023-04-20 15:45:47 +0200

    All Orders Component

---
+++ src/components/AllOrders.jsx
@@ -0,0 +1,51 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import { TableCellCustom } from './TableCellCustom';
+import { TableRowCustom } from './TableRowCustom';
+import data from '../data.js'
+
+const tableStyle = {
+    minWidth: '95%',
+    maxWidth: '95%',
+}
+
+function TableComp(){
+    return(
+    <TableContainer className="tableContainer adminPageContent flex wrap centerX shadow">
+        <div className="box header flex">
+            <p className="text header">Zamówienia</p>
+        </div>
+      <Table sx={tableStyle} size="small" aria-label="a dense table">
+            <TableHead>
+            <TableRowCustom>
+                <TableCellCustom align="center">ID</TableCellCustom>
+                <TableCellCustom align="center">Data</TableCellCustom>
+                <TableCellCustom align="center">Koszt</TableCellCustom>
+                <TableCellCustom align="center">Status</TableCellCustom>
+                <TableCellCustom align="center">ID Klienta</TableCellCustom>
+            </TableRowCustom>
+            </TableHead>
+            <TableBody>
+            {data.orders.map((row) => (
+                <TableRowCustom
+                key={row.id}
+                sx ={{'&:last-of-type': { border: 0 }, 
+                '&:hover':{bgcolor: '#F5F5F5'},
+                }}
+                >
+                <TableCellCustom align="center">{row.id}</TableCellCustom>
+                <TableCellCustom align="center">{row.date}</TableCellCustom>
+                <TableCellCustom align="center">{row.cost}</TableCellCustom>
+                <TableCellCustom align="center">{row.status}</TableCellCustom>
+                <TableCellCustom align="center">{row.customer}</TableCellCustom>
+                </TableRowCustom>
+            ))}
+            </TableBody>
+      </Table>
+    </TableContainer>
+    );
+}
+
+export default TableComp;
