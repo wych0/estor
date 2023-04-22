188c0cf20d767dab2e14b96c31e8f6921e3638a9
wych0 <wycho01@wp.pl>
2023-04-22 12:54:40 +0200

    Block btn users list

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -5,6 +5,18 @@ import TableHead from '@mui/material/TableHead';
 import { TableCellCustom } from './TableCellCustom';
 import { TableRowCustom } from './TableRowCustom';
 import data from '../data.js'
+import { Button } from '@mui/material';
+
+const btnStyle = {
+    width: '90%',
+    maxWidth: '90px',
+    bgcolor: '#1A1882',
+    '&:hover':{
+        bgcolor: '#283593'
+    },
+    fontSize: 12,
+    fontWeight: "700",
+}
 
 const tableStyle = {
     minWidth: '95%',
@@ -13,7 +25,7 @@ const tableStyle = {
 
 function TableComp(){
     return(
-    <TableContainer className="tableContainer adminPageContent flex wrap centerX shadow">
+    <TableContainer className="tableContainer users adminPageContent flex wrap centerX shadow">
         <div className="box header flex">
             <p className="text header">Użytkownicy</p>
         </div>
@@ -22,6 +34,8 @@ function TableComp(){
             <TableRowCustom>
                 <TableCellCustom align="center">ID</TableCellCustom>
                 <TableCellCustom align="center">Nazwa</TableCellCustom>
+                <TableCellCustom align="center">Status konta</TableCellCustom>
+                <TableCellCustom align="center"></TableCellCustom>
             </TableRowCustom>
             </TableHead>
             <TableBody>
@@ -34,6 +48,8 @@ function TableComp(){
                 >
                 <TableCellCustom align="center">{row.id}</TableCellCustom>
                 <TableCellCustom align="center">{row.name}</TableCellCustom>
+                <TableCellCustom align="center">{row.accStatus}</TableCellCustom>
+                <TableCellCustom align="center"><Button variant="contained" sx={btnStyle}>Zablokuj</Button></TableCellCustom>
                 </TableRowCustom>
             ))}
             </TableBody>
