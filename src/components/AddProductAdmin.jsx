0bab455eb78ddecd1601efcb6e9d66a63efa12bd
wych0 <wycho01@wp.pl>
2023-04-22 16:02:59 +0200

    Makin preview image in admin adding item panel

--- src/components/AddProductAdmin.jsx
+++ src/components/AddProductAdmin.jsx
@@ -2,8 +2,7 @@ import { Button } from '@mui/material';
 import Input from './Input';
 
 const btnStyle = {
-    width: '90%',
-    maxWidth: '90px',
+    width: '125px',
     bgcolor: '#1A1882',
     '&:hover':{
         bgcolor: '#283593'
@@ -22,16 +21,37 @@ const formStyle = {
 
 
 function AddProductAdmin(){
+    const previewImage = (e) => {
+        const imageFiles = e.target.files
+        if(imageFiles.length > 0){
+            const imgSrc = URL.createObjectURL(imageFiles[0])
+            const imagePreviewElement = document.querySelector('.previewImg')
+            imagePreviewElement.src = imgSrc
+            imagePreviewElement.style.display="block"
+        }
+    }
     return(
         <div className="addProductContainer flex shadow wrap centerX">
             <div className="box header flex">
                 <p className="text header">Dodaj produkt</p>
             </div>
-            <div className="inputsAddProduct">
-                <Input formStyle={formStyle} placeHolder="Marka"/>
-                <Input formStyle={formStyle} placeHolder="Nazwa"/>
-                <Input formStyle={formStyle} placeHolder="Cena"/>
+            <div className="addProductBody flex wrap">
+                <div className="inputsAddProduct addProductContent">
+                    <Input formStyle={formStyle} placeHolder="Marka"/>
+                    <Input formStyle={formStyle} placeHolder="Nazwa"/>
+                    <Input formStyle={formStyle} placeHolder="Cena"/>
+                </div>
+                <div className="imgAddContainer addProductContent">
+                <div className="boxImgAddAdmin shadow flex centerX centerY">
+                    <img className="previewImg"/>
+                </div>
+                <Button variant="contained" component="label" sx={btnStyle}>
+                    Dodaj zdjęcie
+                    <input hidden type="file" accept="image/*" onChange={(e) => previewImage(e)} />
+                </Button>
+                </div>
             </div>
+            
             
         </div>
        
