7095b7601b6b78f5efc279bd7fc7de223aae61a1
wych0 <wycho01@wp.pl>
2023-04-24 19:41:08 +0200

    New table row customed

---
+++ src/components/TableRowHead.jsx
@@ -0,0 +1,6 @@
+import {styled} from "@mui/material/styles";
+import TableRow from '@mui/material/TableRow';
+
+export const TableRowHead = styled(TableRow)(({ theme }) => ({
+    borderBottom: '1px solid rgba(26,24,130,0.3);',
+}));
