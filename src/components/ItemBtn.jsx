7b609ac94b831ed92d606fa5d890d7fff93f91a5
wych0 <wycho01@wp.pl>
2023-04-24 19:38:41 +0200

    Code refactoring

--- src/components/ItemBtn.jsx
+++ src/components/ItemBtn.jsx
@@ -2,6 +2,18 @@ import Button from '@mui/material/Button';
 import { useContext} from 'react';
 import { UserContext } from '../UserContext';
 
+const btnItemStyle = {
+    color: 'black',
+    height: '20px',
+    width: '60px',
+    fontSize: 12,
+    textTransform: 'none',
+    "@media (max-width:1600px)": {
+      fontSize: 11,
+    },
+  }
+
+
 function ItemBtn(props) {
   const {user, addItemToCart, deleteItemFromCart} = useContext(UserContext);
   const cartItemsIds = user.cart.map((item) => item.id);
@@ -14,29 +26,12 @@ function ItemBtn(props) {
     cartItemsIds.includes(props.itemId) ? deleteItemFromCart(props.itemId) : addItemToCart(product)
   }
 
-  const btnItemStyle = {
-    color: btnFunction === 'add' ? 'black' : 'black',
-    '&:hover':{
-        color: btnFunction === 'add' ? 'black' : 'black',
-    },
-    maxHeight: '20px',
-    maxWidth: '60px',
-    minHeight: '20px',
-    minWidth: '60px',
-    fontSize: 12,
-    display: user.auth ? 'flex' : 'none',
-    textTransform: 'none',
-    "@media (max-width:1600px)": {
-      fontSize: 11,
-    },
-  }
-
+  
     return (
       <div>
-        <Button onClick={handleClick} sx={btnItemStyle}>
-          <i className={icon+" icon itemBtn"}></i>
-          {text}
-        </Button>
+        {user.auth 
+        ? <Button onClick={handleClick} sx={btnItemStyle}><i className={icon+" icon itemBtn"}></i>{text}</Button> 
+        : ' '}
       </div>
     );
   }
