2ee989fc33744ccc41f0abadc8d024c946724f71
wych0 <wycho01@wp.pl>
2023-04-24 19:40:42 +0200

    Custom Table

---
+++ src/components/TableCustom.jsx
@@ -0,0 +1,7 @@
+import {styled} from "@mui/material/styles";
+import Table from '@mui/material/Table';
+
+export const TableCustom = styled(Table)(({ theme }) => ({
+    minWidth: '95%',
+    maxWidth: '95%',
+}));
