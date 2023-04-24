3b10c3d8a8ac2b3ed1f654ce1fc14dcde480000d
wych0 <wycho01@wp.pl>
2023-04-24 19:40:58 +0200

    Code refactoring

--- src/components/TableRowCustom.jsx
+++ src/components/TableRowCustom.jsx
@@ -3,4 +3,6 @@ import TableRow from '@mui/material/TableRow';
 
 export const TableRowCustom = styled(TableRow)(({ theme }) => ({
     borderBottom: '1px solid rgba(26,24,130,0.3);',
+    '&:hover':{backgroundColor: '#F5F5F5'},
+    '&:last-of-type': { border: 0 }
 }));
