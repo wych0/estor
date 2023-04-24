3e34b34b92e8058d26a3c11b83c2fdbb8d52cb41
wych0 <wycho01@wp.pl>
2023-04-24 19:37:41 +0200

    Code refactoring

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -1,27 +1,12 @@
-import Table from '@mui/material/Table';
+import { TableCustom } from './TableCustom';
 import TableBody from '@mui/material/TableBody';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import { TableCellCustom } from './TableCellCustom';
 import { TableRowCustom } from './TableRowCustom';
 import data from '../data.js'
-import { Button } from '@mui/material';
-
-const btnStyle = {
-    width: '90%',
-    maxWidth: '90px',
-    bgcolor: '#1A1882',
-    '&:hover':{
-        bgcolor: '#283593'
-    },
-    fontSize: 12,
-    fontWeight: "700",
-}
-
-const tableStyle = {
-    minWidth: '95%',
-    maxWidth: '95%',
-}
+import TableBtn from './TableBtn'
+import { TableRowHead } from './TableRowHead';
 
 function TableComp(){
     return(
@@ -29,33 +14,32 @@ function TableComp(){
         <div className="box header flex">
             <p className="text header">Produkty</p>
         </div>
-      <Table sx={tableStyle} size="small" aria-label="a dense table">
+      <TableCustom size="small" aria-label="a dense table">
             <TableHead>
-            <TableRowCustom>
+            <TableRowHead>
                 <TableCellCustom align="center">ID</TableCellCustom>
                 <TableCellCustom align="center">Marka</TableCellCustom>
                 <TableCellCustom align="center">Nazwa</TableCellCustom>
                 <TableCellCustom align="center">Cena</TableCellCustom>
+                <TableCellCustom align="center">Status</TableCellCustom>
                 <TableCellCustom align="center"></TableCellCustom>
-            </TableRowCustom>
+            </TableRowHead>
             </TableHead>
             <TableBody>
             {data.products.map((row) => (
                 <TableRowCustom
                 key={row.id}
-                sx ={{'&:last-of-type': { border: 0 }, 
-                '&:hover':{bgcolor: '#F5F5F5'},
-                }}
                 >
                 <TableCellCustom align="center">{row.id}</TableCellCustom>
                 <TableCellCustom align="center">{row.brand}</TableCellCustom>
                 <TableCellCustom align="center">{row.name}</TableCellCustom>
                 <TableCellCustom align="center">{row.price}</TableCellCustom>
-                <TableCellCustom align="center"><Button variant="contained" sx={btnStyle}>Usuń</Button></TableCellCustom>
+                <TableCellCustom align="center">{row.isSold ? 'Sprzedany' : 'Dostępny'}</TableCellCustom>
+                <TableCellCustom align="center"><TableBtn variant="contained">Usuń</TableBtn></TableCellCustom>
                 </TableRowCustom>
             ))}
             </TableBody>
-      </Table>
+      </TableCustom>
     </TableContainer>
     );
 }
