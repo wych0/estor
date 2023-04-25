7863eabee7064e3d0c3e4c1c414532a0e8bea83c
wych0 <wycho01@wp.pl>
2023-04-25 15:10:30 +0200

    Code refactoring

--- src/components/CompleteContainer.jsx
+++ src/components/CompleteContainer.jsx
@@ -1,28 +1,7 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../UserContext';
 import {Link } from "react-router-dom";
-import Button from '@mui/material/Button';
-
-const btnStyle = {
-    marginTop: 3,
-    marginBottom: 3,
-    height: 50,
-    width: 400,
-    bgcolor: '#1A1882',
-    '&:hover':{
-        bgcolor: '#283593'
-    },
-    fontSize: 16,
-    fontWeight: "700",
-    "@media (max-width:1600px)": {
-      marginTop: 5,
-      marginBottom: 2.5,
-      height: 45,
-      width: 300,
-      fontSize: 13,
-      fontWeight: "600",
-    },
-  } 
+import { CompleteBtn } from './CompleteBtn';
 
 function CompleteContainer() {
     const {endOrderProcess} = useContext(UserContext);
@@ -31,7 +10,7 @@ function CompleteContainer() {
         <i className="bi bi-check-circle-fill icon complete"></i>
         <p className="text complete big">Otrzymaliśmy twoje zamówienie</p>
         <p className="text complete small">Dziękujemy za korzystanie z naszych usług</p>
-        <Link to={"/auth"}><Button variant="contained" sx={btnStyle} onClick={endOrderProcess}>Wróć do strony głównej</Button></Link>
+        <Link to={"/auth"}><CompleteBtn variant="contained" onClick={endOrderProcess}>Wróć do strony głównej</CompleteBtn></Link>
         
       </div>
     );
