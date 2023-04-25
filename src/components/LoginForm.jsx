00d0aa1ef7e8db8a6a7a3f933b5e4990fca5491c
wych0 <wycho01@wp.pl>
2023-04-25 15:11:19 +0200

    Code refactoring

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,106 +1,42 @@
 import React, { useContext } from 'react';
 import {Link } from "react-router-dom";
 import { UserContext } from '../UserContext';
-import IconButton from '@mui/material/IconButton';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import EmailIcon from '@mui/icons-material/Email';
-import HttpsIcon from '@mui/icons-material/Https';
 import { SignupLoginBtn } from './SignupLoginBtn';
+import InputPassword from './InputPassword';
+import Input from './Input';
 
 const formStyle = {
   mb: 2.5,
-    width: '52ch',
+    width: '45ch',
     "@media (max-width:1600px)": {
       width: '45ch',
     },
 }
 
-const inputStyle = {
-    color: '#1A1882',
-    borderColor: '#1A1882',
-    fontWeight: "500",
-    "@media (max-width:1600px)": {
-      height:"50px",
-    },
-}
-
 const labelStyle = {
-    color: '#1A1882',
-    "&.Mui-focused":{
-        color: '#1A1882',
-        fontWeight: "700"
-    },
-    fontWeight: "500",
-    fontSize: 20,
-    paddingRight: 1,
-    bgcolor: 'white',
+  color: '#1A1882',
+  "&.Mui-focused":{
+      color: '#1A1882',
+      fontWeight: "700",
+  },
+  fontWeight: "500",
+  fontSize: 17,
 }
 
-const iconStyle = {
-    color: '#1A1882'
-}
 
 
 function LoginForm() {
     const {login} = useContext(UserContext);
-    const [showPassword, setShowPassword] = React.useState(false);
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
 
     return (
       <div className="boxForm login flex wrap centerX shadow">
-        <div className="boxLogoWelcome flex wrap centerX centerY">
-            <Link to={'/'}><img src="images/logoSvg.svg" alt="logo" className="logoImgForm"/></Link>
+        <div className="boxWelcome flex wrap centerX centerY">
+            <Link className="linkLogoImg login" to={'/'}><img src="images/logoSvg.svg" alt="logo" className="logoImgForm"/></Link>
             <p className="text welcome">Witamy ponownie!</p>
         </div>
         <div className="form flex">
-        <FormControl sx={formStyle} variant="outlined">
-          <InputLabel htmlFor="outlined-with-icon-adornment" sx={labelStyle} shrink={true}>Adres e-mail</InputLabel>
-          <OutlinedInput
-            startAdornment={
-                <InputAdornment position="start">
-                <EmailIcon sx={iconStyle} />
-                </InputAdornment>
-            }
-            sx={inputStyle}
-            id="outlined"
-            label="Adres e-mail"
-          />
-        </FormControl>
-        <FormControl sx={formStyle} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password-with-icon-adornment" sx={labelStyle}>Hasło</InputLabel>
-          <OutlinedInput
-            sx={inputStyle}
-            id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            startAdornment={
-                <InputAdornment position="start">
-                <HttpsIcon sx={iconStyle} />
-                </InputAdornment>
-            }
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                  sx={iconStyle}
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Hasło"
-          />
-        </FormControl>
+        <Input startIcon={true} formStyle={formStyle} labelStyle={labelStyle} placeHolder="E-mail" />
+        <InputPassword startIcon={true} formStyle={formStyle} labelStyle={labelStyle} placeHolder="Hasło"/>
         </div>
         <Link to={"/auth"}><SignupLoginBtn variant="contained" onClick={login}>Zaloguj się</SignupLoginBtn></Link>
       </div>
