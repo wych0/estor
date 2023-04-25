c510c0bc6c54324a425bf8ea48fd2f7ebf3d56ea
wych0 <wycho01@wp.pl>
2023-04-25 15:10:44 +0200

    Code refactoring

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,31 +1,37 @@
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import EmailIcon from '@mui/icons-material/Email';
+
   const inputStyle = {
     height:"50px",
     color: '#1A1882',
     borderColor: '#1A1882',
     fontWeight: "500",
   }
-  
-  const labelStyle = {
-    color: '#1A1882',
-    "&.Mui-focused":{
-        color: '#1A1882',
-        fontWeight: "700",
-    },
-    fontWeight: "400",
-    fontSize: 14,
-  }
+
+  const iconStyle = {
+    color: '#1A1882'
+}
+
 
 function Input(props) {
+  const iconAdornment = props.startIcon 
+    ? {
+      startAdornment: <InputAdornment position="start">
+        <EmailIcon sx={iconStyle} />
+      </InputAdornment>
+    }
+    : { }
     return (
       <div className="formContainer">
         <FormControl sx={props.formStyle} variant="outlined">
-            <InputLabel htmlFor="outlined" sx={labelStyle}>{props.placeHolder}</InputLabel>
+            <InputLabel htmlFor="outlined" sx={props.labelStyle}>{props.placeHolder}</InputLabel>
             <OutlinedInput
               sx={inputStyle}
-              label={props.placeHolder}
+              label={props.placeHolder+"."}
+              {...iconAdornment}
             />
           </FormControl>
       </div>
