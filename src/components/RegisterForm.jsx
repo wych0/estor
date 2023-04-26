35b3caddee8845812b6f988c4878c6568e1d0a20
Albert Wychowaniec <wycho01@wp.pl>
2023-04-26 12:18:26 +0200

    Styling

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -2,11 +2,9 @@ import React, { useContext } from 'react';
 import {Link } from "react-router-dom";
 import { UserContext } from '../UserContext';
 import { SignupLoginBtn } from './SignupLoginBtn';
-import Input from './Input';
-import InputPassword from './InputPassword';
+import { Input } from './Input';
 
 const formStyle = {
-  mb: 2,
     width: '45ch',
     "@media (max-width:1600px)": {
       width: '40ch',
@@ -18,7 +16,7 @@ const labelStyle = {
   "&.Mui-focused":{
       color: '#1A1882',
       fontWeight: "700",
-      fontSize: 18
+      fontSize: 17
   },
   fontWeight: "400",
   fontSize: 15,
@@ -31,14 +29,14 @@ function RegisterForm() {
       <div className="boxForm register flex wrap centerX shadow">
         <div className="boxWelcome register flex wrap centerX">
             <Link className="linkLogoImg register" to={'/'}><img src="images/logoSvg.svg" alt="logo" className="logoImgForm"/></Link>
-            <p className="text register">Nie masz jeszcze konta?</p>
+            <p className="text register top">Nie masz jeszcze konta?</p>
             <p className="text register bottom">Zarejestruj się korzystając formularza poniżej</p>
         </div>
-        <div className="form flex">
+        <div className="form register flex">
         <Input formStyle={formStyle} labelStyle={labelStyle} placeHolder="Imię"/>
         <Input formStyle={formStyle} labelStyle={labelStyle} placeHolder="Nazwisko"/>
         <Input formStyle={formStyle} labelStyle={labelStyle} placeHolder="E-mail"/>
-        <InputPassword startIcon={false} labelStyle={labelStyle} formStyle={formStyle} placeHolder="Hasło"/>
+        <Input isPassword={true} formStyle={formStyle} labelStyle={labelStyle} placeHolder="Hasło" />
         </div>
         <Link to={"/auth"}><SignupLoginBtn variant="contained" onClick={login}>Zarejestruj się</SignupLoginBtn></Link>
       </div>
