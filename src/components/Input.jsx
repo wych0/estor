c097e3c5d50fa46688df1dd55e5ccb4f36ce3c52
Albert Wychowaniec <wycho01@wp.pl>
2023-04-26 12:17:23 +0200

    Merging input and password input into one component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,12 +3,16 @@ import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import InputAdornment from '@mui/material/InputAdornment';
 import EmailIcon from '@mui/icons-material/Email';
+import HttpsIcon from '@mui/icons-material/Https';
+import { IconButton } from '@mui/material';
+import { VisibilityOff } from '@mui/icons-material';
+import Visibility from '@mui/icons-material/Visibility';
+import { useState } from 'react';
 
   const inputStyle = {
     height:"50px",
     color: '#1A1882',
     borderColor: '#1A1882',
-    fontWeight: "500",
   }
 
   const iconStyle = {
@@ -16,26 +20,62 @@ import EmailIcon from '@mui/icons-material/Email';
 }
 
 
-function Input(props) {
-  const iconAdornment = props.startIcon 
+export const Input = ({isPassword, isStartIcon, formStyle, labelStyle, placeHolder}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+  const startAdornment = isStartIcon
     ? {
       startAdornment: <InputAdornment position="start">
-        <EmailIcon sx={iconStyle} />
+        {isPassword
+        ? <HttpsIcon sx={iconStyle} />
+        : <EmailIcon sx={iconStyle} />}
       </InputAdornment>
     }
     : { }
+
+  const typeInput = isPassword
+  ? {
+    type: showPassword ? 'text' : 'password'
+  }
+  : { }
+
+  const endAdornment = isPassword 
+  ? {
+    endAdornment: <InputAdornment position="end">
+      <IconButton
+        aria-label="toggle password visibility"
+        onClick={handleClickShowPassword}
+        onMouseDown={handleMouseDownPassword}
+        edge="end"
+        sx={iconStyle}
+      >
+        {showPassword ? <VisibilityOff /> : <Visibility />}
+      </IconButton>
+    </InputAdornment>
+    }
+  : { }
+  
     return (
       <div className="formContainer">
-        <FormControl sx={props.formStyle} variant="outlined">
-            <InputLabel htmlFor="outlined" sx={props.labelStyle}>{props.placeHolder}</InputLabel>
+        <FormControl sx={formStyle} variant="outlined">
+            <InputLabel htmlFor="outlined" sx={labelStyle}>{placeHolder}</InputLabel>
             <OutlinedInput
               sx={inputStyle}
-              label={props.placeHolder+"."}
-              {...iconAdornment}
+              label={placeHolder+"."}
+              {...startAdornment}
+              {...typeInput}
+              {...endAdornment}
             />
           </FormControl>
       </div>
     );
   }
+
+  const InputError = () => {
+    return <div>error</div>
+  }
   
-  export default Input;
