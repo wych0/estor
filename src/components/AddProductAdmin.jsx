51d91412e3ae959bbb5dc998a5bdb59fbd9f986c
Albert Wychowaniec <wycho01@wp.pl>
2023-04-27 17:34:53 +0200

    Admin panel inputs validation

--- src/components/AddProductAdmin.jsx
+++ src/components/AddProductAdmin.jsx
@@ -1,24 +1,19 @@
 import { Input } from './Input';
 import { MidBtn } from './MidBtn';
 import { SmallBtn } from './SmallBtn';
+import {FormProvider, useForm} from 'react-hook-form'
 
 const formStyle = {
     width: '20ch',
 }
 
-const labelStyle = {
-  color: '#1A1882',
-  "&.Mui-focused":{
-      color: '#1A1882',
-      fontWeight: "700",
-  },
-  fontWeight: "400",
-  fontSize: 15,
-}
-
-
-
 function AddProductAdmin(){
+    const methods = useForm()
+
+    const onSubmit = methods.handleSubmit( (data) => {
+      console.log(data);
+    })
+
     const previewImage = (e) => {
         const imageFiles = e.target.files
         if(imageFiles.length > 0){
@@ -30,14 +25,16 @@ function AddProductAdmin(){
     }
     return(
         <div className="addProductContainer flex shadow wrap centerX">
+            <FormProvider {...methods}>
+            <form className="flex wrap centerX" onSubmit={e => e.preventDefault()} noValidate>
             <div className="box header flex">
                 <p className="text header">Dodaj produkt</p>
             </div>
             <div className="addProductBody flex wrap">
                 <div className="inputsAddProduct flex wrap addProductContent">
-                    <Input formStyle={formStyle} labelStyle={labelStyle} placeHolder="Marka"/>
-                    <Input formStyle={formStyle} labelStyle={labelStyle} placeHolder="Nazwa"/>
-                    <Input formStyle={formStyle} labelStyle={labelStyle} placeHolder="Cena"/>
+                    <Input formStyle={formStyle} placeHolder="Marka"/>
+                    <Input formStyle={formStyle} placeHolder="Nazwa"/>
+                    <Input formStyle={formStyle} placeHolder="Cena"/>
                 </div>
                 <div className="imgAddContainer addProductContent">
                 <div className="boxImgAddAdmin shadow flex centerX centerY">
@@ -49,11 +46,13 @@ function AddProductAdmin(){
                 </SmallBtn>
                 </div>
                 <div className="addProductBtnContainer flex centerX">
-                    <MidBtn variant="contained">Dodaj produkt</MidBtn>
+                    <MidBtn variant="contained" onClick={onSubmit}>Dodaj produkt</MidBtn>
                 </div>
+               
             </div>
             
-            
+            </form>
+            </FormProvider>
         </div>
        
     );
