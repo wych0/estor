0535faae611c607b23b4e82d4c1c256e2dbee164
Albert Wychowaniec <wycho01@wp.pl>
2023-04-27 17:36:02 +0200

    Ship details validation inputs

--- src/components/ShipDetails.jsx
+++ src/components/ShipDetails.jsx
@@ -1,21 +1,13 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../UserContext';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
-import {Link } from "react-router-dom";
 import Button from '@mui/material/Button';
 import { Input } from './Input';
-import {FormProvider} from 'react-hook-form'
+import {FormProvider, useForm} from 'react-hook-form'
+import { useNavigate } from 'react-router-dom';
 
 const formStyle = {
   width: '21ch',
-}
-
-const formStyleMedium = {
-  width: '27ch',
-}
-
-const formStyleSmall = {
-  width: '15ch',
 }
 
 const formStyleBig = {
@@ -25,17 +17,6 @@ const formStyleBig = {
 const iconStyle = {
   color: '#1A1882',
   fontSize: '50px',
-}
-
-const labelStyle = {
-  color: '#1A1882',
-  "&.Mui-focused":{
-      color: '#1A1882',
-      fontWeight: "700",
-      fontSize: 16,
-  },
-  fontWeight: "400",
-  fontSize: 15,
 }
 
 const btnStyle = {
@@ -51,7 +32,16 @@ const btnStyle = {
 
 function ShipDetails() {
     const {user, placeOrder} = useContext(UserContext)
+    const methods = useForm()
+    const navigation = useNavigate()
     const isBtnDisabled = user.cart.length===0 ? true : false
+
+    const onSubmit = methods.handleSubmit( (data) => {
+      placeOrder()
+      navigation("/complete")
+    })
+
+
     return (
       <div className="bodyCartContent shipDetailsCart flex centerX shadow">
         <div className="shipDetailsFormContainer flex wrap centerX">
@@ -59,18 +49,18 @@ function ShipDetails() {
             <LocalShippingIcon sx={iconStyle}/> 
             <p className="text summaryCartHeaders">&nbsp;Dane do wysyłki</p>
           </div>
-          <FormProvider>
-            <form >
+          <FormProvider {...methods}>
+            <form className="flex wrap centerX" onSubmit={e => e.preventDefault()} noValidate>
               <div className="formShipDetails flex wrap">
-                <Input formStyle={formStyle} labelStyle={labelStyle} placeHolder="Imię"/>
-                <Input formStyle={formStyle} labelStyle={labelStyle} placeHolder="Nazwisko"/>
-                <Input formStyle={formStyleBig} labelStyle={labelStyle} placeHolder="Ulica, numer domu"/>
-                <Input formStyle={formStyleMedium} labelStyle={labelStyle} placeHolder="Miasto"/>
-                <Input formStyle={formStyleSmall} labelStyle={labelStyle} placeHolder="Kod-Pocztowy"/>
-                <Input formStyle={formStyleBig} labelStyle={labelStyle} placeHolder="Adres e-mail"/>
-                <Input formStyle={formStyleMedium} labelStyle={labelStyle} placeHolder="Kraj"/>
+                <Input formStyle={formStyle} placeHolder="Imię"/>
+                <Input formStyle={formStyle}  placeHolder="Nazwisko"/>
+                <Input formStyle={formStyleBig}  placeHolder="Ulica, numer domu"/>
+                <Input formStyle={formStyle}  placeHolder="Miasto"/>
+                <Input formStyle={formStyle} placeHolder="Kod-Pocztowy"/>
+                <Input formStyle={formStyleBig} placeHolder="Adres e-mail"/>
+                <Input formStyle={formStyle} placeHolder="Kraj"/>
               </div>
-              <Button disabled={isBtnDisabled} component={Link} to={"/complete"} variant="contained" sx={btnStyle} onClick={placeOrder}>Zamów z obowiązkiem zapłaty</Button>
+              <Button disabled={isBtnDisabled} variant="contained" sx={btnStyle} onClick={onSubmit}>Zamów z obowiązkiem zapłaty</Button>
             </form>
           </FormProvider>
         </div>
