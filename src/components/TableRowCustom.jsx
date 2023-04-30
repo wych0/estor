6bfb867855a29d227d5e921a2016699e668d5f90
wych0 <wycho01@wp.pl>
2023-04-30 12:39:34 +0200

    Improving the catalogue structure

--- src/components/TableRowCustom.jsx
+++
@@ -1,8 +0,0 @@
-import {styled} from "@mui/material/styles";
-import TableRow from '@mui/material/TableRow';
-
-export const TableRowCustom = styled(TableRow)(({ theme }) => ({
-    borderBottom: '1px solid rgba(26,24,130,0.3);',
-    '&:hover':{backgroundColor: '#F5F5F5'},
-    '&:last-of-type': { border: 0 }
-}));
