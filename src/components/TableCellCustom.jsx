f338ce6df4af5d442b4428b04744ebdb784c8e42
wych0 <wycho01@wp.pl>
2023-04-30 12:39:17 +0200

    Improving the catalogue structure

--- src/components/TableCellCustom.jsx
+++
@@ -1,6 +0,0 @@
-import {styled} from "@mui/material/styles";
-import TableCell from '@mui/material/TableCell';
-
-export const TableCellCustom = styled(TableCell)(({ theme }) => ({
-    border: 0,
-}));
