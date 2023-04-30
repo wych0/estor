93c2f25a6a6a0f0c85eefc1e8bf2cca6101c3e41
wych0 <wycho01@wp.pl>
2023-04-30 12:39:29 +0200

    Improving the catalogue structure

--- src/components/TableCustom.jsx
+++
@@ -1,7 +0,0 @@
-import {styled} from "@mui/material/styles";
-import Table from '@mui/material/Table';
-
-export const TableCustom = styled(Table)(({ theme }) => ({
-    minWidth: '95%',
-    maxWidth: '95%',
-}));
