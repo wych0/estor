6de4659a75214776600109640c3ce868c2999b8b
wych0 <wycho01@wp.pl>
2023-05-01 15:50:11 +0200

    One file for all buttons to easier accesibility

--- src/components/ImageInput.jsx
+++ src/components/ImageInput.jsx
@@ -1,7 +1,7 @@
-import { useFormContext } from 'react-hook-form';
-import { findInputErrors } from '../findInputErrors';
-import { isFormValid } from '../isFormValid';
-import { SmallBtn } from './SmallBtn';
+import { useFormContext } from 'react-hook-form'
+import { findInputErrors } from '../findInputErrors'
+import { isFormValid } from '../isFormValid'
+import { SmallBtn } from './Buttons'
 
 const previewImage = (e) => {
     const imageFiles = e.target.files
