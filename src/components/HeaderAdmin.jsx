cea46d6266485dd6336db36aeada16c0b93e77f3
wych0 <wycho01@wp.pl>
2023-05-01 15:31:12 +0200

    Changing catalogue struture

--- src/components/HeaderAdmin.jsx
+++
@@ -1,9 +0,0 @@
-function HeaderAdmin() {
-    return (
-      <div className={"headerContainer flex wrap centerY centerX"}>
-        <img src="images/logoSvg.svg" alt="logoImgHeader" className="logoImgHeader"/>
-      </div>
-    );
-  }
-  
-  export default HeaderAdmin;
