714ff36297ffde3771aa01e0cb2403175c3c7671
wych0 <wycho01@wp.pl>
2023-05-02 13:33:03 +0200

    Improving catalogue structure

---
+++ src/components/CartPageComponents/Item.jsx
@@ -0,0 +1,17 @@
+import ItemBtn from '../ItemBtn'
+
+export default function Item(props) {
+    return (
+      <div className="boxItemCart flex wrap">
+        <div className="boxImgCart shadow flex centerX centerY">
+            <img className="imgItem" src="/images/products/product1.png" alt="produkt"/>
+        </div>
+        <div className="boxBrandNameBtnCart flex wrap">
+          <p className="text itemCart brand">{props.brand}</p>
+          <p className="text itemCart name">{props.name}</p>
+          <ItemBtn itemId={props.itemId}></ItemBtn>
+        </div>
+        <p className="text itemCart price">{props.price} zł</p>
+      </div>
+    );
+}
