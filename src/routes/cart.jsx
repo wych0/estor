672540fedf0678e5762151917a82df0b8172326e
wych0 <wycho01@wp.pl>
2023-05-02 13:35:07 +0200

    Improving catalogue structure

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -1,11 +1,11 @@
 import Header from '../components/Header'
-import BodyCart from '../components/BodyCart'
+import { Body } from '../components/CartPageComponents/Body'
 
 export default function Cart() {
     return (
         <div id="cartPage" className="page">
         <Header></Header>
-        <BodyCart></BodyCart>
+        <Body></Body>
         </div>
     );
 }
