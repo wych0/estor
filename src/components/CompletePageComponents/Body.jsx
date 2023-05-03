b0f503bfa452ff3379893853817ca6af2d4be2c6
wych0 <wycho01@wp.pl>
2023-05-03 12:34:57 +0200

    Improving catalogue structure

---
+++ src/components/CompletePageComponents/Body.jsx
@@ -0,0 +1,11 @@
+import CompleteContainer from "./CompleteContainer"
+
+export default function Body() {
+    return (
+      <div className="bodyPage bodyCompletePage flex centerX centerY">
+        <CompleteContainer></CompleteContainer>
+      </div>
+    );
+  }
+
+
