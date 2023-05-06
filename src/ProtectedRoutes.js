bb9194cde7dd8a1910bf0db5969e93845ab1eb6e
wych0 <wycho01@wp.pl>
2023-05-06 13:52:31 +0200

    Improving catalogue structure

---
+++ src/ProtectedRoutes.js
@@ -0,0 +1,22 @@
+import { Navigate } from "react-router-dom"
+
+export const ProtectedAdmin = ({ role, children }) => {
+    if (role!=='admin') {
+        return <Navigate to="/login" replace />
+    }
+    return children
+};
+
+export const ProtectedAuth = ({ isLoggedIn, children }) => {
+    if (!isLoggedIn) {
+        return <Navigate to="/login" replace />
+    }
+    return children
+};
+
+export const ProtectedComplete = ({ placedOrder, children }) => {
+    if (!placedOrder) {
+        return <Navigate to="/auth" replace />
+    }
+    return children
+};
