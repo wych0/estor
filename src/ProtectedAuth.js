6e08e3dda6e98de745381cbe692d109e3388df4f
wych0 <wycho01@wp.pl>
2023-05-06 13:52:41 +0200

    Improving catalogue structure

--- src/ProtectedAuth.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-const ProtectedAuth = ({ isLoggedIn, children }) => {
-  if (!isLoggedIn) {
-    return <Navigate to="/login" replace />;
-  }
-  return children;
-};
-export default ProtectedAuth;
