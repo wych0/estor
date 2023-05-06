ebfb1e5ae4337749cc0481b2ad7a5b7b0f51c49f
wych0 <wycho01@wp.pl>
2023-05-06 13:52:46 +0200

    Improving catalogue structure

--- src/ProtectedAdmin.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-const ProtectedAdmin = ({ role, children }) => {
-  if (role!=='admin') {
-    return <Navigate to="/login" replace />;
-  }
-  return children;
-};
-export default ProtectedAdmin;
