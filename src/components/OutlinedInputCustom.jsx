370bccadec3f6ad5ed8ddb7e84f4c10209de96b6
wych0 <wycho01@wp.pl>
2023-05-08 16:50:04 +0200

    Code refactoring

--- src/components/OutlinedInputCustom.jsx
+++ src/components/OutlinedInputCustom.jsx
@@ -1,7 +1,7 @@
-import {styled} from "@mui/material/styles";
-import OutlinedInput from '@mui/material/OutlinedInput';
+import {styled} from "@mui/material/styles"
+import OutlinedInput from '@mui/material/OutlinedInput'
 
-export const OutlinedInputCustom = styled(OutlinedInput)(({ theme, isvalid }) => ({
+export const OutlinedInputCustom = styled(OutlinedInput)(({ isvalid }) => ({
     '& .MuiOutlinedInput-notchedOutline, &.Mui-focused .MuiOutlinedInput-notchedOutline, &:hover .MuiOutlinedInput-notchedOutline': {
         borderColor: isvalid ? "#1A1882" : "#C1292E",
     },
