443ac8deddf32976cbe944ff1fb7f0ca24062cba
wych0 <wycho01@wp.pl>
2023-05-08 16:49:41 +0200

    Code refactoring

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { UserProvider } from './UserContext';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { UserProvider } from './UserContext'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
       <UserProvider>
