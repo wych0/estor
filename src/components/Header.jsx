f412d3b90994c3a1f5462f957c95ee46b21063c1
wych0 <wycho01@wp.pl>
2023-05-17 17:01:50 +0200

    Making logout call to backend

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,26 @@ import {Link } from "react-router-dom"
 import React, { useContext } from 'react'
 import { UserContext } from '../UserContext'
 import { MidBtn, NavButton } from './Buttons'
+import axios from 'axios'
 
 export default function Header(props) {
     const { user, logout } = useContext(UserContext)
     const centerLogo = props.childDisplay === 'none' ? 'centerX' : 'spaceBetween'
+
+    const onClick = (async() => {
+      try{
+        const response = await axios.post('http://localhost:8000/auth/logout',{},{withCredentials: true})
+        console.log(response.data)
+      } catch(error){
+        console.log(error.response.data.message)
+      }
+    })
+
     return (
       <div className={"headerContainer flex centerY " + centerLogo}>
         <div className={"headerContent flex centerX "+props.childDisplay}>
           {user.auth
-          ?  <Link to={"/"}><MidBtn variant="contained" onClick={logout}>Wyloguj się</MidBtn></Link>
+          ?  <Link to={"/"}><MidBtn variant="contained" onClick={onClick}>Wyloguj się</MidBtn></Link>
           :  <p className="text shipInfo free">Bezpłatna dostawa i zwrot!</p>
           }
         </div>
