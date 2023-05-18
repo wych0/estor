ee4ad3d0375a8d12bf396ca1a12a2a3ff575139b
wych0 <wycho01@wp.pl>
2023-05-18 16:23:55 +0200

    Started working on displaying errors from backend

--- src/components/SignInUpPageComponents/LoginForm.jsx
+++ src/components/SignInUpPageComponents/LoginForm.jsx
@@ -6,19 +6,24 @@ import { Input } from '../Input'
 import {useForm} from 'react-hook-form'
 import { FormProvider } from 'react-hook-form'
 import { login } from '../../apiCalls/auth'
+import { useState } from 'react'
+import Alert from '@mui/material/Alert'
+import IconButton from '@mui/material/IconButton'
+import CloseIcon from '@mui/icons-material/Close'
 
 export default function LoginForm() {
     const methods = useForm()
-    const {user, setAuth} = useContext(UserContext)
+    const {user, loginUser} = useContext(UserContext)
     const navigation = useNavigate()
+    const [error, setError] = useState()
 
     const onSubmit = methods.handleSubmit(async (data) => {
       try{
         const result = await login(data["E-mail"], data["Hasło"])
         console.log(result.message)
-        setAuth(true, result.role)
+        loginUser(result.role)
       } catch(error){
-        console.log(error)
+        setError(error)
       } 
     })
 
@@ -36,6 +41,25 @@ export default function LoginForm() {
 
     return (
       <div className="boxForm login flex wrap centerX shadow">
+        {error
+          ? <Alert
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          {error}
+        </Alert>
+        : ''
+
+        }
+        
         <div className="boxWelcome flex wrap centerX centerY">
             <Link className="linkLogoImg login" to={'/'}><img src="images/logoSvg.svg" alt="logo" className="logoImgForm"/></Link>
             <p className="text welcome">Witamy ponownie!</p>
@@ -44,7 +68,7 @@ export default function LoginForm() {
         <form className="flex wrap centerX" onSubmit={e => e.preventDefault()} noValidate onKeyPress={handleKeyPress} >
         <div className="form login flex">
           <Input isEmail={true} isStartIcon={true} placeHolder="E-mail"/>
-          <Input name='password' isPassword={true} isStartIcon={true} placeHolder="Hasło" />
+          <Input isPassword={true} isStartIcon={true} placeHolder="Hasło" />
         </div>
         <SignupLoginBtn variant="contained" onClick={onSubmit}>Zaloguj się</SignupLoginBtn>
         </form>
