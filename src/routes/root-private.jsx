900022a00bdf12e64353a83f95eb51b8a46929e6
wych0 <wycho01@wp.pl>
2023-05-18 15:22:55 +0200

    renaming file

---
+++ src/routes/root-private.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header'
+import Body from '../components/HomePageComponents/Body'
+
+export default function HomePageLoggedIn() {
+    return (
+      <div id="homeAuthPage" className="page">
+        <Header></Header>
+        <Body></Body>
+      </div>
+    );
+  }
