4a084c221412a0096d548c6d82262e590b573abc
wych0 <wycho01@wp.pl>
2023-05-19 15:03:17 +0200

    Working registration

--- src/components/SignInUpPageComponents/RegisterForm.jsx
+++ src/components/SignInUpPageComponents/RegisterForm.jsx
@@ -6,17 +6,32 @@ import { Input } from '../Input'
 import {useForm} from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 import { FormProvider } from 'react-hook-form'
+import { register } from '../../apiCalls/auth'
+import { useEffect, useState } from 'react'
+import { AlertAuth } from '../Alerts'
 
 function RegisterForm() {
+    const [error, setError] = useState()
     const methods = useForm()
-    const {login} = useContext(UserContext)
+    const {loginUser, user} = useContext(UserContext)
     const navigation = useNavigate()
 
-    const onSubmit = methods.handleSubmit( (data) => {
-      console.log(data);
-      login()
-      navigation("/auth")
+    const onSubmit = methods.handleSubmit( async (data) => {
+      try{
+        const result = await register(data['Imię'], data['Nazwisko'], data['E-mail'], data['Hasło'])
+        loginUser('klient')
+      } catch(error){
+        if(error){
+          setError(error)
+        }
+      }
     })
+
+    useEffect(()=>{
+      if(user.auth){
+        navigation('/')
+      } 
+    }, [user.auth, navigation])
 
     const handleKeyPress = (e) => {
       if (e.key === 'Enter') {
@@ -31,11 +46,15 @@ function RegisterForm() {
             <p className="text register top">Nie masz jeszcze konta?</p>
             <p className="text register bottom">Zarejestruj się korzystając formularza poniżej</p>
         </div>
+        {error
+          ? <AlertAuth icon={false}><i className="bi bi-exclamation-circle-fill icon error"></i> {error}</AlertAuth>
+          : ''
+        }
         <FormProvider {...methods}>
         <form className="flex wrap centerX" onSubmit={e => e.preventDefault()} noValidate onKeyPress={handleKeyPress}>
         <div className="form register flex">
-        <Input placeHolder="Imię"/>
-        <Input placeHolder="Nazwisko"/>
+        <Input isLettersOnly={true} placeHolder="Imię"/>
+        <Input isLettersOnly={true} placeHolder="Nazwisko"/>
         <Input isEmail={true} placeHolder="E-mail"/>
         <Input isPassword={true} isRegister={true} placeHolder="Hasło" />
         </div>
