71ec81659d19d414eed06ffb2c74c12c126f61a2
wych0 <wycho01@wp.pl>
2023-05-21 22:19:59 +0200

    Making deleting and adding product to cart working

--- src/components/ItemBtn.jsx
+++ src/components/ItemBtn.jsx
@@ -1,7 +1,7 @@
 import Button from '@mui/material/Button'
 import { useContext, useState, useEffect} from 'react'
 import { UserContext } from '../UserContext'
-import { addItem, deleteItem, getCart } from '../apiCalls/cart'
+import { addItem, deleteItem} from '../apiCalls/cart'
 
 const btnItemStyle = {
     color: 'black',
@@ -14,28 +14,30 @@ const btnItemStyle = {
     },
   }
 
-
   export default function ItemBtn(props) {
-  const {user} = useContext(UserContext)
+  const {user, setCart} = useContext(UserContext)
   const [btnFunction, setFunction] = useState('add')
   const icon = btnFunction==='add' ? 'bi bi-cart-plus-fill' : 'bi bi-cart-dash-fill'
   const text = btnFunction==='add' ? 'Dodaj' : 'Usuń'
 
   const handleClick = () => {
     if(btnFunction==='add'){
-      addItem(props.itemId)
+      addItem(props.itemId).then((product)=>{
+      setCart([...user.cart, product])
       setFunction('delete')
-    }else{
-      deleteItem(props.itemId)
-      setFunction('add')
+    })}else{
+      deleteItem(props.itemId).then(()=>{
+        const updatedCart = user.cart.filter((item)=>item._id !== props.itemId)
+        setCart(updatedCart)
+        setFunction('add')
+      })
     }
   }
 
-  useEffect(()=>{
-    user.cart.includes(props.itemId) ? setFunction('delete') : setFunction('add')
-  },[user.cart, props.itemId])
+  useEffect(() => {
+    user.cart.some((item) => item._id === props.itemId) ? setFunction('delete') : setFunction('add')
+  }, [user.cart, props.itemId])
 
-  
     return (
       <div>
         {user.auth 
