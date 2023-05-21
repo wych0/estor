37d7ebd2f62942db01e511c78e177c0c88c839c9
wych0 <wycho01@wp.pl>
2023-05-21 16:04:36 +0200

    displaying items from database

--- src/components/HomePageComponents/Items.jsx
+++ src/components/HomePageComponents/Items.jsx
@@ -1,20 +1,25 @@
 import Item from './Item'
-import data from '../../data.js'
+import { getProducts } from '../../apiCalls/product'
+import { useState, useEffect } from 'react'
 
 function matchesSearchTags(el, searchTags){
-  const tags = el.brand.concat(" ", el.name);
-  return searchTags.every(tag => tags.toLowerCase().includes(tag));
+  const tags = el.brand.concat(" ", el.name)
+  return searchTags.every(tag => tags.toLowerCase().includes(tag))
 }
 
 export default function Items(props) {
-  const searchTags = props.input ? props.input.split(" ") : [];
-  const filterData = data.products.filter((el => matchesSearchTags(el, searchTags) && !el.isSold));
+  const searchTags = props.input ? props.input.split(" ") : []
+  const [products, setProducts] = useState([])
+  useEffect(()=>{
+    getProducts().then((products)=> setProducts(products))
+  }, [])
+  const filterData = products.filter((el => matchesSearchTags(el, searchTags) && !el.isSold))
 
     return (
       <div className="boxItems"> 
         <ul className="listItems flex wrap centerX">
           {filterData.map((item) => (
-            <li key={item.id} className="listItem"><Item itemId={item.id} brand={item.brand} name={item.name} price={item.price}></Item></li>
+            <li key={item._id} className="listItem"><Item itemId={item._id} brand={item.brand} name={item.name} price={item.price}></Item></li>
           ))}
         </ul> 
       </div>
