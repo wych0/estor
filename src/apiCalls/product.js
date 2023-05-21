a3f773fa814f76a825d4610bf449a3f37fc9afeb
wych0 <wycho01@wp.pl>
2023-05-21 16:04:25 +0200

    getting product and all prodcuts

---
+++ src/apiCalls/product.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+export const getProducts = async()=>{
+    try{
+        const response = await axios.get('http://localhost:8000/product/all', {withCredentials: true})
+        return Promise.resolve(response.data.products)
+    } catch(error){
+        return Promise.reject(error.response.data.message)
+    }
+}
+
+export const getProduct = async(productID)=>{
+    try{
+        const response = await await axios.get('http://localhost:8000/product/all', 
+        {productID: productID},
+        {withCredentials: true})
+        return Promise.resolve(response.data.product)
+    } catch(error){
+        return Promise.reject(error.response.data.message)
+    }
+}
+
