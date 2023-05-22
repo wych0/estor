beee913892878bc16444c9203ef244ddb710c158
wych0 <wycho01@wp.pl>
2023-05-22 20:59:07 +0200

    displaying user orders from backend

--- src/components/AccountPageComponents/UserOrders.jsx
+++ src/components/AccountPageComponents/UserOrders.jsx
@@ -1,14 +1,19 @@
-import data from '../../data.js'
 import { UserContext } from '../../UserContext'
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import TableBody from '@mui/material/TableBody'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import {TableBtn, TableRowHead, TableRowCustom, TableCellCustom, TableCustom} from '../TableComponents'
+import { getUserOrders } from '../../apiCalls/order.js'
 
 export default function UserOders(){
+    const [userOrders, setUserOrders] = useState([])
     const {displayOrderDetails, user} = useContext(UserContext)
-    const userOrders = data.orders.filter(el => el.customer===user.id)
+
+    useEffect(()=>{
+        getUserOrders(user.id).then((orders)=>setUserOrders(orders))
+    },[user.id, setUserOrders])
+
     return(
     <TableContainer className="pageContent accPage tableContainer flex wrap centerX shadow">
         <div className="box header flex">
@@ -27,12 +32,12 @@ export default function UserOders(){
             <TableBody>
             {userOrders.map((row) => (
                 <TableRowCustom
-                key={row.id}
+                key={row._id}
                 sx ={{'&:last-of-type': { border: 0}, 
                 '&:hover':{bgcolor: '#F5F5F5'},
                 }}
                 >
-                <TableCellCustom align="center">{row.id}</TableCellCustom>
+                <TableCellCustom align="center">{row._id.slice(15)}</TableCellCustom>
                 <TableCellCustom align="center">{row.date}</TableCellCustom>
                 <TableCellCustom align="center">{row.cost}</TableCellCustom>
                 <TableCellCustom align="center">{row.status}</TableCellCustom>
