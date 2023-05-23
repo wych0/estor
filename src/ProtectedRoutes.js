f4baf181bf688b916c9d7f851eee9243d91ed3b1
wych0 <wycho01@wp.pl>
2023-05-23 16:02:07 +0200

    protecting router in other way

--- src/ProtectedRoutes.js
+++ src/ProtectedRoutes.js
@@ -1,12 +1,5 @@
 import { Navigate } from "react-router-dom"
 
-export const ProtectedAuth = ({ isLoggedIn, children }) => {
-    if (!isLoggedIn) {
-        return <Navigate to="/login" replace />
-    }
-    return children
-};
-
 export const ProtectedComplete = ({ placedOrder, children }) => {
     if (!placedOrder) {
         return <Navigate to="/" replace />
