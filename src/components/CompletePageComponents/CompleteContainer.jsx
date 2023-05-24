4cebc0a0efe80cc4720781bc44e5d46168cfc8e3
Albert Wychowaniec <wycho01@wp.pl>
2023-05-24 13:18:20 +0200

    changing setPlacedOrder to false after placing order

--- src/components/CompletePageComponents/CompleteContainer.jsx
+++ src/components/CompletePageComponents/CompleteContainer.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom"
 import { BigBtn } from '../Buttons'
+import { useContext } from "react"
+import { UserContext } from "../../UserContext"
 
 export default function CompleteContainer() {
+  const {setPlacedOrder} = useContext(UserContext)
+  const handleClick = (e)=>{
+    e.preventDefault()
+    setPlacedOrder(false)
+  }
     return (
       <div className="completeBox flex wrap centerX shadow">
         <i className="bi bi-check-circle-fill icon complete"></i>
         <p className="text complete big">Otrzymaliśmy twoje zamówienie</p>
         <p className="text complete small">Dziękujemy za korzystanie z naszych usług</p>
         <div className="completeBtn container flex centerX">
-          <BigBtn component={Link} to={"/"} variant="contained">Wróć do strony głównej</BigBtn>
+          <BigBtn component={Link} to={"/"} variant="contained" onClick={(e) => handleClick(e)}>Wróć do strony głównej</BigBtn>
         </div>
       </div>
     );
