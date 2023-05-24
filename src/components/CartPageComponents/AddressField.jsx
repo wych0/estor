41aafa514734f3202e916f04dca228581cda5288
Albert Wychowaniec <wycho01@wp.pl>
2023-05-24 14:16:45 +0200

    pretending displaying address field wile valueVar is undefined

--- src/components/CartPageComponents/AddressField.jsx
+++ src/components/CartPageComponents/AddressField.jsx
@@ -8,13 +8,17 @@ const textFieldStyle = {
   },
 };
 
-export const AddressField = ({formStyle, label}) => {
+export const AddressField = ({formStyle, label, valueVar}) => {
+  if(!valueVar){
+    return null
+  }
+    console.log(valueVar)
     return (
       <div className="inputContainer">
         <FormControl sx={formStyle} variant="outlined">
         <TextField
           id="outlined-read-only-input"
-          defaultValue="Hello World"
+          defaultValue={valueVar}
           label={label}
           InputProps={{
             readOnly: true,
