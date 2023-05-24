0add13864bfb942cdef2c3be4c9ef158ef3528a7
Albert Wychowaniec <wycho01@wp.pl>
2023-05-24 13:17:21 +0200

    switching request to user api call

--- src/apiCalls/auth.js
+++ src/apiCalls/auth.js
@@ -36,21 +36,3 @@ export const register = async(name, secName, email, password)=>{
     return Promise.reject(error.response.data.error)
   }
 }
-
-export const getRole = async()=>{
-  try{
-    const response = await axios.get('http://localhost:8000/auth/role', {withCredentials: true})
-    return Promise.resolve(response.data.role)
-  } catch(error){
-    return Promise.reject(error.response.data.error)
-  }
-}
-
-export const isAuth = async()=>{
-  try{
-    const response = await axios.get('http://localhost:8000/auth', {withCredentials: true})
-    return Promise.resolve(response.data.isAuth)
-  } catch(error){
-    return Promise.reject(error.response.data.error)
-  }
-}
