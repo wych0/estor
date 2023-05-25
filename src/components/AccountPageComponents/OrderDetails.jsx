77f0edef0344a002eb0d77bcd2f7716051af4429
Albert Wychowaniec <wycho01@wp.pl>
2023-05-25 17:48:55 +0200

    Displaying order details working with backend

--- src/components/AccountPageComponents/OrderDetails.jsx
+++ src/components/AccountPageComponents/OrderDetails.jsx
@@ -1,18 +1,16 @@
-import data from '../../data.js'
-import { useContext } from "react"
+import { useContext, useState, useEffect} from "react"
 import { UserContext } from "../../UserContext"
 import { ItemOrderDetails } from './ItemOrderDetails'
 import { SmallBtn } from '../Buttons'
-
-function filterProductsByOrderItems(products, orderItems) {
-    return products.filter((product) => orderItems.includes(product.id));
-  }
+import { getOrder } from '../../apiCalls/order.js'
 
 export function OrderDetails(props){
+    const [order, setOrder] = useState()
     const {user} = useContext(UserContext)
-    const orderToDisplay = data.orders.find(el => el.id===user.displayedOrder)
-    const isOrderChosen = props.isOrderChosen==='false' ? false : true
-    if(!isOrderChosen) {
+    useEffect(()=>{
+        getOrder(user.displayedOrder).then((order)=>setOrder(order))
+    },[order, user.displayedOrder])
+    if(!order) {
         return (
             <div className="box orderDetailsContent flex wrap centerX">
                 <div className="box orderDetailsHeader header flex centerY">
@@ -25,36 +23,33 @@ export function OrderDetails(props){
             </div>
         )
     }
-    const filterData = filterProductsByOrderItems(data.products, orderToDisplay.products)
-    const isBtnDisabled = orderToDisplay.status==="W realizacji" ? false : true
+    // const isBtnDisabled = orderToDisplay.status==="W realizacji" ? false : true
     return(
         <div className="box orderDetailsContent flex wrap centerX">
             <div className="box orderDetailsHeader header flex centerY">
                 <p className="text orderDetails header">Szczegóły zamówienia</p>
-                <SmallBtn variant="contained" disabled={isBtnDisabled}>Anuluj</SmallBtn>
+                <SmallBtn variant="contained" disabled={false}>Anuluj</SmallBtn>
             </div>
                 <div className="box displayOrderDetails flex wrap">
                     <div className="box orderDetailsInfo flex wrap">
-                        <p className="text orderDetails info">Numer: <span className="text orderDetails info data">{orderToDisplay.id}</span></p>
-                        <p className="text orderDetails info">Data: <span className="text orderDetails info data">{orderToDisplay.date}</span></p>
-                        <p className="text orderDetails info">Koszt: <span className="text orderDetails info data">{orderToDisplay.cost}</span></p>
-                        <p className="text orderDetails info">Status: <span className="text orderDetails info data">{orderToDisplay.status}</span></p>
+                        <p className="text orderDetails info">Numer: <span className="text orderDetails info data">{order._id}</span></p>
+                        <p className="text orderDetails info">Data: <span className="text orderDetails info data">{order.date}</span></p>
+                        <p className="text orderDetails info">Koszt: <span className="text orderDetails info data">{order.cost}</span></p>
+                        <p className="text orderDetails info">Status: <span className="text orderDetails info data">{order.status}</span></p>
                     </div>
                     <div className="box orderDetailsInfo flex wrap">
                         <p className="text orderDetails info">Adres dostawy</p>
-                        <p className="text orderDetails info data address">{orderToDisplay.addres.name.concat((' '), orderToDisplay.addres.secName)}</p>
-                        <p className="text orderDetails info data address">{orderToDisplay.addres.street}</p>
-                        <p className="text orderDetails info data address">{orderToDisplay.addres.postalCode.concat((', '), orderToDisplay.addres.city)}</p>
-                        <p className="text orderDetails info data address">{orderToDisplay.addres.country}</p>
+                        <p className="text orderDetails info data address">{order.address.name.concat((' '), order.address.secName)}</p>
+                        <p className="text orderDetails info data address">{order.address.street}</p>
+                        <p className="text orderDetails info data address">{order.address.postalCode.concat((', '), order.address.city)}</p>
+                        <p className="text orderDetails info data address">{order.address.country}</p>
                     </div>
                     <div className ="orderDetailsItems flex wrap">
                         <p className="text orderDetails info">Zamówione produkty</p>
-                        {filterData===' '
-                        ? <p> </p>
-                        : filterData.map((item) => (
-                            <div key={item.id} className="listItemOrderDetails"><ItemOrderDetails itemId={item.id} brand={item.brand} name={item.name} price={item.price}></ItemOrderDetails></div>
-                            ))
-                        } 
+                        {order.products.map((item) => (
+                            <div key={item._id} className="listItemOrderDetails"><ItemOrderDetails brand={item.brand} name={item.name} price={item.price}></ItemOrderDetails></div>
+                            ))}
+                    
                     </div>
             </div> 
         </div>
