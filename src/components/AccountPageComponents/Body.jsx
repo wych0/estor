610661d051dd66fe681a5df01c068a5905783ffa
wych0 <wycho01@wp.pl>
2023-05-26 19:36:45 +0200

    Working on update date in two sibling components

--- src/components/AccountPageComponents/Body.jsx
+++ src/components/AccountPageComponents/Body.jsx
@@ -1,12 +1,18 @@
 import * as React from 'react'
 import UserOrders from './UserOrders'
-import OrderDetailsContainer from './OrderDetailsContainer'
+import OrderDetails from './OrderDetails'
+import { useState } from 'react'
 
 export function Body() {
+    const [orderCancelled, setOrderCancelled] = useState(false)
+
+    const handleOrderCancellation = ()=>{
+      setOrderCancelled(true)
+    }    
     return (
       <div className="bodyPage accPage flex wrap centerX">
-        <UserOrders></UserOrders>
-        <OrderDetailsContainer></OrderDetailsContainer>
+        <UserOrders orderCancelled={orderCancelled}></UserOrders>
+        <OrderDetails onOrderCancellation={handleOrderCancellation}></OrderDetails>
       </div>
     );
   }
