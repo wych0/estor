cb444018633279d08b9eed16532406bdf579b5ad
wych0 <wycho01@wp.pl>
2023-05-26 19:36:24 +0200

    Added dialog component after cancelling order

--- src/components/AccountPageComponents/OrderDetails.jsx
+++ src/components/AccountPageComponents/OrderDetails.jsx
@@ -1,17 +1,31 @@
 import { useContext, useState, useEffect} from "react"
 import { UserContext } from "../../UserContext"
 import { ItemOrderDetails } from './ItemOrderDetails'
-import { SmallBtn } from '../Buttons'
-import { getOrder } from '../../apiCalls/order.js'
+import { SmallBtn, DialogButton } from '../Buttons'
+import { getOrder, cancelOrder } from '../../apiCalls/order.js'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
 
-export function OrderDetails(props){
+export default function OrderDetails({onOrderCancellation }){
     const [order, setOrder] = useState()
+    const [open, setOpen] = useState(false)
+    const [buttonDisabled, setDisabled] = useState(null)
+    const [orderStatus, setOrderStatus] = useState(null)
     const {user} = useContext(UserContext)
+
     useEffect(()=>{
-        getOrder(user.displayedOrder).then((order)=>setOrder(order))
-    },[order, user.displayedOrder])
+        getOrder(user.displayedOrder).then((order)=>{
+            setOrder(order)
+            setOrderStatus(order.status)
+            order.status==='W realizacji' ? setDisabled(false) : setDisabled(true)})
+    },[user.displayedOrder])
+
     if(!order) {
         return (
+        <div className="box pageContent accPage orderDetails flex wrap shadow">
             <div className="box orderDetailsContent flex wrap centerX">
                 <div className="box orderDetailsHeader header flex centerY">
                     <p className="text orderDetails header">Nie wybrano zamówienia</p>
@@ -21,21 +35,57 @@ export function OrderDetails(props){
                     <p className="text detailsBtn info">Aby wyświetlić szczegóły danego zamówienia, skorzystaj z przycisku "Szczegóły".</p>
                 </div>
             </div>
+        </div>
         )
     }
-    // const isBtnDisabled = orderToDisplay.status==="W realizacji" ? false : true
+
+    const handleClickOpen = () => {
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    const handleSubmit = () =>{
+        cancelOrder(order._id).then(console.log('Anulowano zamówienie'))
+        setDisabled(true)
+        setOrderStatus('Anulowane')
+        onOrderCancellation()
+        setOpen(false)
+    }
     return(
+    <div className="box pageContent accPage orderDetails flex wrap shadow">
         <div className="box orderDetailsContent flex wrap centerX">
             <div className="box orderDetailsHeader header flex centerY">
                 <p className="text orderDetails header">Szczegóły zamówienia</p>
-                <SmallBtn variant="contained" disabled={false}>Anuluj</SmallBtn>
+                <SmallBtn variant="contained" disabled={buttonDisabled} onClick={handleClickOpen}>Anuluj</SmallBtn>
+                <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                    {"Czy na pewno chcesz anulować zamówienie?"}
+                    </DialogTitle>
+                    <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Anulowanie zamówienia jest akcją, której nie można cofnąć
+                    </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                    <DialogButton onClick={handleSubmit}>Tak</DialogButton>
+                    <DialogButton onClick={handleClose}>Nie</DialogButton>
+                    </DialogActions>
+                </Dialog>
             </div>
                 <div className="box displayOrderDetails flex wrap">
                     <div className="box orderDetailsInfo flex wrap">
                         <p className="text orderDetails info">Numer: <span className="text orderDetails info data">{order._id}</span></p>
                         <p className="text orderDetails info">Data: <span className="text orderDetails info data">{order.date}</span></p>
                         <p className="text orderDetails info">Koszt: <span className="text orderDetails info data">{order.cost}</span></p>
-                        <p className="text orderDetails info">Status: <span className="text orderDetails info data">{order.status}</span></p>
+                        <p className="text orderDetails info">Status: <span className="text orderDetails info data">{orderStatus}</span></p>
                     </div>
                     <div className="box orderDetailsInfo flex wrap">
                         <p className="text orderDetails info">Adres dostawy</p>
@@ -53,5 +103,6 @@ export function OrderDetails(props){
                     </div>
             </div> 
         </div>
+    </div>
     );
 }
