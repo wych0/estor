008b3d5374801b709281ec3fe30f8ae6d4789679
Albert Wychowaniec <wycho01@wp.pl>
2023-05-31 10:52:01 +0200

    Items list working with backend

--- src/components/AdminPageComponents/TableItems.jsx
+++ src/components/AdminPageComponents/TableItems.jsx
@@ -1,15 +1,38 @@
 import { useState, useEffect } from 'react'
-import { getProducts } from '../../apiCalls/product.js'
+import { getProducts, deleteProduct } from '../../apiCalls/product.js'
 import TableBody from '@mui/material/TableBody'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import {TableBtn, TableRowHead, TableRowCustom, TableCellCustom, TableCustom} from '../TableComponents'
+import ConfirmationDialog from '../ConfirmationDialog'
 
 export function TableItems(){
     const [products, setProducts] = useState([])
+    const [dialogOpen, setDialogOpen] = useState(false)
+    const [productID, setProductID] = useState(null)
     useEffect(()=>{
         getProducts().then((products)=>setProducts(products))
     },[])
+
+    const handleClickOpen = (productID) => {
+        setProductID(productID)
+        setDialogOpen(true)
+    }
+
+    const handleSubmit = async () => {
+        try{
+            await deleteProduct(productID)
+            getProducts().then((products)=>setProducts(products))
+        } catch(error){
+            getProducts().then((products)=>setProducts(products))
+        }
+    }
+
+    const handleClose = () => {
+        setDialogOpen(false)
+    }
+
+
     return(
     <TableContainer className="tableContainer products flex wrap centerX shadow">
         <div className="box header flex">
@@ -29,18 +52,25 @@ export function TableItems(){
             <TableBody>
             {products.map((row) => (
                 <TableRowCustom
-                key={row.id}
+                key={row._id}
                 >
-                <TableCellCustom align="center">{row._id}</TableCellCustom>
+                <TableCellCustom align="center">{row._id.slice(15)}</TableCellCustom>
                 <TableCellCustom align="center">{row.brand}</TableCellCustom>
                 <TableCellCustom align="center">{row.name}</TableCellCustom>
                 <TableCellCustom align="center">{row.price}</TableCellCustom>
                 <TableCellCustom align="center">{row.isSold ? 'Sprzedany' : 'Dostępny'}</TableCellCustom>
-                <TableCellCustom align="center"><TableBtn variant="contained">Usuń</TableBtn></TableCellCustom>
+                <TableCellCustom align="center"><TableBtn onClick={()=>handleClickOpen(row._id)} variant="contained">Usuń</TableBtn></TableCellCustom>
                 </TableRowCustom>
             ))}
             </TableBody>
       </TableCustom>
+      <ConfirmationDialog
+        open={dialogOpen}
+        onClose={handleClose}
+        onConfirm={handleSubmit}
+        title="Czy na pewno chcesz usunąć produkt?"
+        description="Usunięcie produktu jest akcją, której nie można cofnąć"
+        />
     </TableContainer>
     );
 }
