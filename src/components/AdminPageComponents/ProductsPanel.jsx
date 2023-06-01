64288e7915b23d3f7957fe6aba077c6e976736e7
wych0 <wycho01@wp.pl>
2023-06-01 21:51:34 +0200

    make state to know when reload items list

--- src/components/AdminPageComponents/ProductsPanel.jsx
+++ src/components/AdminPageComponents/ProductsPanel.jsx
@@ -1,11 +1,16 @@
 import { AddProductForm } from "./AddProductForm"
 import { TableItems } from "./TableItems"
+import { useState } from "react"
 
 export function ProductsPanel() {
+    const [productAdded, setProductAdded] = useState(false)
+      const onProductAdd = ()=>{
+        setProductAdded(true)
+      }    
     return (
       <div className="flex wrap pageContent adminPage adminProducts">
-        <TableItems></TableItems>
-        <AddProductForm></AddProductForm>
+        <TableItems productAdded={productAdded} setProductAdded={setProductAdded}></TableItems>
+        <AddProductForm onProductAdd={onProductAdd}></AddProductForm>
     </div>
     );
   }
