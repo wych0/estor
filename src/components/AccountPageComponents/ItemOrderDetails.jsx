5e004ca34ccb599ca7653677df4ae896c6e9894b
wych0 <wycho01@wp.pl>
2023-06-01 21:50:21 +0200

    importing product image from server

--- src/components/AccountPageComponents/ItemOrderDetails.jsx
+++ src/components/AccountPageComponents/ItemOrderDetails.jsx
@@ -2,7 +2,7 @@ export function ItemOrderDetails(props) {
     return (
       <div className="boxItemOrderDetails flex wrap">
         <div className="boxImgOrderDetails shadow flex centerX centerY">
-            <img className="imgItem" src="/images/products/product1.png" alt="produkt"/>
+            <img className="imgItem" src={`http://localhost:8000/images/${props.image}`} alt="produkt"/>
         </div>
         <div className="boxItemOrderDetailsProps flex wrap">
           <p className="text itemOrderDetails brand">{props.brand}</p>
